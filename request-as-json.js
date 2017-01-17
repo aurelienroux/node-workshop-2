1192b30a13eef19d72d4c4827845dfb81885249c
Aurélien Roux <aureroux13@me.com>
2017-01-17 19:42:52 +0000

    request json file done

---
+++ request-as-json.js
@@ -0,0 +1,24 @@
+var request = require('request');
+
+function requestJson(url, callback){
+   request(url, function (error, response) {
+      if (error) {
+         callback(error); 
+      } else {
+         try{
+            var answer = JSON.parse(response.body);
+            callback(null, answer);
+         } catch(error) {
+            callback(error);
+         }
+      }
+   });
+}
+
+requestJson("http://api.open-notify.org/iss-now.json", function(err, result){
+   if(err){
+      console.log("this is error", err);
+   } else {
+      console.log("this is result", result);
+   }
+});
