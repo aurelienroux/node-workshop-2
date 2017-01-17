2fdabba8b7ba352111d8e30a3ed733c29b4a2bc8
Aurélien Roux <aureroux13@me.com>
2017-01-17 21:59:08 +0000

    weather forecast working

---
+++ darkSkyAPI.js
@@ -0,0 +1,31 @@
+var prompt = require("prompt");
+var requestJSON = require('./library/request-json');
+
+
+prompt.start();
+prompt.get("userLocation", function (err, userResult) {
+    if(err){
+        console.log("prompt error is:", err);
+    } else {
+        console.log("your location is: " + userResult.userLocation);
+        requestJSON("https://maps.googleapis.com/maps/api/geocode/json?address=" + userResult.userLocation, function(errReqOne, resultReqOne){
+            if(errReqOne){
+                console.log("this is user location error" + errReqOne);
+            } else {
+                var userLat = resultReqOne.results[0].geometry.location.lat;
+                var userLng = resultReqOne.results[0].geometry.location.lng;
+                requestJSON("https://api.darksky.net/forecast/8b3d826a8192d73fe0d71085a9b2ab06/" + userLat + "," + userLng, function(errReqTwo, resultReqTwo){
+                    if(errReqTwo){
+                        console.log("this is weather app error" + errReqTwo);
+                    } else {
+                        var dailyWeatherData = resultReqTwo.daily.data;
+                        console.log("Weather is actually " + resultReqTwo.currently.summary);
+                        dailyWeatherData.forEach(function(ele, index){
+                            console.log("in " + (index+1) + " days, weather will be " + ele.icon);
+                        });
+                    }
+                });//end of weather request
+            }
+        }); // end of city request
+    }// end of else prompt
+});// end of prompt
