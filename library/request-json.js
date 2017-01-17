8ac1ded8a68c2545101b845b4b5d696c7b8327f6
Aurélien Roux <aureroux13@me.com>
2017-01-17 21:58:38 +0000

    changes to request module and requestJson file

--- library/request-json.js
+++ library/request-json.js
@@ -15,10 +15,3 @@ module.exports = function requestJson(url, callback){
    });
 }
 
-// requestJson("http://api.open-notify.org/iss-now.json", function(err, result){
-//   if(err){
-//       console.log("this is error", err);
-//   } else {
-//       console.log("this is result", result);
-//   }
-// });
--- request-as-json.js
+++ request-as-json.js
@@ -1,6 +1,6 @@
 var requestJSON = require('./library/request-json');
 
-requestJSON("http://api.open-notify.org/iss-now.json", function(err, result){
+requestJSON("https://api.darksky.net/forecast/8b3d826a8192d73fe0d71085a9b2ab06/37.8267,-122.4233", function(err, result){
    if(err){
       console.log("this is error", err);
    } else {
