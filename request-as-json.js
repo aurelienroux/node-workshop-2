bd5b53967b9ade8cb6e9f72e01a6759d7e7367d7
Aurélien Roux <aureroux13@me.com>
2017-01-17 20:16:53 +0000

    modified request-as-json to only have function call

--- request-as-json.js
+++ request-as-json.js
@@ -1,24 +1,11 @@
-var request = require('request');
+var requestJSON = require('./library/request-json');
 
-function requestJson(url, callback){
-   request(url, function (error, response) {
-      if (error) {
-         callback(error); 
-      } else {
-         try{
-            var answer = JSON.parse(response.body);
-            callback(null, answer);
-         } catch(error) {
-            callback(error);
-         }
-      }
-   });
-}
-
-requestJson("http://api.open-notify.org/iss-now.json", function(err, result){
+requestJSON("http://api.open-notify.org/iss-now.json", function(err, result){
    if(err){
       console.log("this is error", err);
    } else {
       console.log("this is result", result);
    }
 });
+
+// console.log(myfile);
